50388ada5cb8869db194aea197dca07da321aa70
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-07 21:18:37 -0300

    implements missing methods and makes the tests pass

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(
+      item => item.type === 'income',
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      item => item.type === 'outcome',
+    );
+
+    const income = incomeTransactions.reduce(
+      (total, transaction) => total + transaction.value,
+      0,
+    );
+
+    const outcome = outcomeTransactions.reduce(
+      (total, transaction) => total + transaction.value,
+      0,
+    );
+
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    if (type === 'outcome' && value > this.getBalance().total) {
+      throw Error('The outcome value can not be bigger than income value.');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
